837f190d9fad67a1bb4a2f18efac8b4a0debe5b6
Chelsea Saunders <c_chilcoat@msn.com>
2022-06-28 10:45:11 -0700

    handlbars practie problems

--- lesson_3/practice_problems_handlebars_basics.js
+++ lesson_3/practice_problems_handlebars_basics.js
@@ -1,11 +1,15 @@
-// Handlebars.registerPartial('tag', $('#tag').html());
-Handlebars.registerPartial('tag','im a partial');
-let post = {
+Handlebars.registerPartial('tag', $('#tag').html());
+
+let posts = [ {
   title: 'Lorem ipsum dolor sit amet',
   published: 'April 1, 2015',
   body: 'Sed<br>ut<br><strong>perspiciatis</strong><br>unde<br>omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.',
   tags: ['tag1', 'tag2', 'tag3'],
-};
+}, {
+  title: 'second blog',
+  published: 'June 28, 2022',
+  body: 'this is my second blog',
+}];
 
 let postTemplate = Handlebars.compile($('#post').html());
-$('body').append(postTemplate(post));
+$('body').append(postTemplate(posts));
